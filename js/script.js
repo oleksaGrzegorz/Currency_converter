41d3a312ea702de0153eaada810fa8f1a9d405ee
Grzegorz Oleksa <grzesieq92@gmail.com>
2022-01-26 10:33:46 +0100

    js changed - added usd, gbp to calc

--- js/script.js
+++ js/script.js
@@ -1,14 +1,30 @@
-let euroElement = document.querySelector(".js-euro");
+let amountElement = document.querySelector(".js-amount");
 let formElement = document.querySelector(".js-form");
+let currencyElement = document.querySelector(".js-currency");
 let form__resultElement = document.querySelector(".js-form__result");
 
 formElement.addEventListener("submit", (event) => {
     event.preventDefault();
 
-let euro = euroElement.value;
+    let amount = +amountElement.value;
+    let currency = currencyElement.value;
 
-let form__result = euro * 4.56;
+    let form__result;
 
-form__resultElement.innerText = form__result.toFixed(2);
+    switch (currency) {
+        case "EUR":
+            form__result = amount / 4.59;
+            break;
+
+        case "GBP":
+            form__result = amount / 5.50;
+            break
+
+        case "USD":
+            form__result = amount / 4.07;
+            break;
+    }
+
+    form__resultElement.innerHTML = form__result.toFixed(2);
 
 })
